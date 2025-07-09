1a5489b7d6aa7d14aafd954f19263a418ce6d64e
Kelly1319 <kellyk.vargas@gmail.com>
2025-07-09 18:21:57 -0300

    Update validacion.js

--- javascript/validacion.js
+++ javascript/validacion.js
@@ -1,13 +1,9 @@
+const mostrarError = (input, mensaje) => {
+    const divPadre = input.parentNode;
+    const errorText = divPadre.querySelector('.error-text');
+  divPadre.classList.add('error');
+  errorText.innerText = mensaje;
+}
 
-     const mostrarError = (input, mensaje) => {
-        const divPadre = input.parentNode;
-        const errorText = divPadre.querySelector('.error-text');
-        divPadre.classList.add('error');
-        errorText.innerText = mensaje;
-
-    };
-
-    const input = document.querySelector ('#email');
-    const mensaje = 'Error Ingrese emial';
- 
-    
+const input = document.querySelector('#password');
+const mensaje = 'campo obligatorio';
