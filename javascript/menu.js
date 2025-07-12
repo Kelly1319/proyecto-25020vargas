90b5f6e8d46448a9741b78bb73d5f98560a0e163
Kelly1319 <kellyk.vargas@gmail.com>
2025-07-12 15:28:46 -0300

    Update menu.js

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const menuToggle = document.getElementById('menu-toggle');
   const enlaces = document.getElementById('enlaces');
-
+  
   menuToggle.addEventListener('click', (e) => {
     e.stopPropagation(); // Evita cerrar por clic en el botón
     enlaces.classList.toggle('activo');
   });
 
+  
   enlaces.querySelectorAll('a').forEach(link => {
     link.addEventListener('click', () => {
       enlaces.classList.remove('activo');
     });
   });
 
+ 
   document.addEventListener('click', (e) => {
     if (!enlaces.contains(e.target) && !menuToggle.contains(e.target)) {
       enlaces.classList.remove('activo');
